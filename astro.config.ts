e607899f7afad1cf74a22ea32f726fc66e794964
Ben Packer <ben.paul.ryan.packer@gmail.com>
2025-05-25 16:56:18 -0400

    feat: implement availability page

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
@@ -9,6 +10,7 @@ import { SITE } from "./src/config";
 export default defineConfig({
   site: SITE.website,
   integrations: [
+    mdx(),
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
